e57ae0c1dcf374cab5c716b9da95deb3feb8ddbd
Ritayan Chakraborty <ritayanout@gmail.com>
2020-01-20 14:46:39 +0530

    Block multiple clicks in CustomViewOnClickedListener

--- app/src/main/java/com/edge2/views/CustomViewOnClickedListener.java
+++ app/src/main/java/com/edge2/views/CustomViewOnClickedListener.java
@@ -28,14 +28,19 @@ import androidx.annotation.Nullable;
 
 public class CustomViewOnClickedListener {
     private static final int ANIM_DELAY = 150;
+    private static final int BLOCK_DURATION = 3 * ANIM_DELAY;
 
     private OnClickListener listener;
     private Handler clickHandler;
     private boolean isNoClickInProgress = true;
+    private BlockRunnable blockRunnable;
+    private ClickRunnable clickRunnable;
 
     public CustomViewOnClickedListener(@NonNull OnClickListener listener) {
         this.listener = listener;
         clickHandler = new Handler();
+        blockRunnable = new BlockRunnable();
+        clickRunnable = new ClickRunnable();
     }
 
     public void onClick(int position) {
@@ -44,14 +49,44 @@ public class CustomViewOnClickedListener {
 
     public void onClick(int position, @Nullable View root, @Nullable View view1,
                         @Nullable View view2, @Nullable View view3, @Nullable View view4) {
-        // Block double clicks
         if (isNoClickInProgress) {
             isNoClickInProgress = false;
             // Allow view ripple to play before triggering click
-            clickHandler.postDelayed(() -> {
-                listener.onClick(position, root, view1, view2, view3, view4);
-                isNoClickInProgress = true;
-            }, ANIM_DELAY);
+            clickRunnable.setVars(position, root, view1, view2, view3, view4);
+            clickHandler.postDelayed(clickRunnable, ANIM_DELAY);
+            // Block multiple clicks. BLOCK_DURATION > ANIM_DELAY ensures blocking between
+            // the listener being triggered and the new fragment becoming visible
+            clickHandler.postDelayed(blockRunnable, BLOCK_DURATION);
+        }
+    }
+
+    private class BlockRunnable implements Runnable {
+        @Override
+        public void run() {
+            isNoClickInProgress = true;
+        }
+    }
+
+    private class ClickRunnable implements Runnable {
+        private int position;
+        private View root;
+        private View view1;
+        private View view2;
+        private View view3;
+        private View view4;
+
+        void setVars(int position, View root, View view1, View view2, View view3, View view4) {
+            this.position = position;
+            this.root = root;
+            this.view1 = view1;
+            this.view2 = view2;
+            this.view3 = view3;
+            this.view4 = view4;
+        }
+
+        @Override
+        public void run() {
+            listener.onClick(position, root, view1, view2, view3, view4);
         }
     }
 }
