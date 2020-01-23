68a97441486396f046bd8ccb3b9a17b234cba020
Ritayan Chakraborty <ritayanout@gmail.com>
2020-01-23 18:53:29 +0530

    Only use the prepackaged DB if the version number increases

--- app/src/main/java/com/edge2/data/AppDatabase.java
+++ app/src/main/java/com/edge2/data/AppDatabase.java
@@ -21,6 +21,8 @@ package com.edge2.data;
  */
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import androidx.room.Database;
 import androidx.room.Room;
@@ -33,16 +35,33 @@ import com.edge2.eventdetails.models.EventDetailsModel;
 @Database(entities = {GroupsModel.class, EventCategoryModel.class, EventDetailsModel.class},
         exportSchema = false, version = AppDatabase.DB_VERSION)
 abstract class AppDatabase extends RoomDatabase {
-    public static final int DB_VERSION = 1;
+    /**
+     * Make sure that the online DB version and this always stays in sync.
+     */
+    static final int DB_VERSION = 1;
+    private static final String KEY_DB_VERSION = "offlineDbVersion";
+
     private static AppDatabase appDatabase;
 
     static synchronized AppDatabase getDatabase(Context context) {
-        if (appDatabase == null)
-            appDatabase = Room.databaseBuilder(context.getApplicationContext(),
-                    AppDatabase.class, "events.db")
-                    .createFromAsset("db/events.db")
-                    .fallbackToDestructiveMigration()
-                    .build();
+        if (appDatabase == null) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            int dbVersion = prefs.getInt(KEY_DB_VERSION, 0);
+            boolean createFromAsset = dbVersion < DB_VERSION;
+
+            RoomDatabase.Builder<AppDatabase> builder =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class,
+                            "events.db");
+
+            if (createFromAsset) {
+                builder.createFromAsset("db/events.db");
+                prefs.edit()
+                        .putInt(KEY_DB_VERSION, DB_VERSION)
+                        .apply();
+            }
+
+            appDatabase = builder.fallbackToDestructiveMigration().build();
+        }
         return appDatabase;
     }
 
