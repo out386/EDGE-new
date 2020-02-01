04d5fcc1ef2c57757726bdfa24278c73b1f8644f
Ritayan Chakraborty <ritayanout@gmail.com>
2020-02-01 22:52:00 +0530

    Add a splash screen

---
+++ app/src/main/java/com/edge2/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.edge2;
+
+/*
+ * Copyright (C) 2020 Ritayan Chakraborty <ritayanout@gmail.com>
+ *
+ * This file is part of EDGE-new
+ *
+ * EDGE-new is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EDGE-new is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EDGE-new.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        GifImageView splashView = findViewById(R.id.splash_gif);
+        GifDrawable splash = GifDrawable.createFromResource(getResources(), R.raw.edge_splash);
+        if (splash != null) {
+            splash.setLoopCount(1);
+            splash.addAnimationListener(loop -> launchMain());
+        } else {
+            launchMain();
+        }
+        splashView.setImageDrawable(splash);
+
+    }
+
+    private void launchMain() {
+        Intent intent = new Intent(this, MainActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
