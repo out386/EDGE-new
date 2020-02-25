64eaa411a47e45a409e8259a4f1fa50638b5c0db
Ritayan Chakraborty <ritayanout@gmail.com>
2020-02-25 14:48:34 +0530

    Add a points table for Campus ambassadors

--- app/src/main/java/com/edge2/ca/CAFragment.java
+++ app/src/main/java/com/edge2/ca/CAFragment.java
@@ -45,6 +45,7 @@ import com.edge2.transitions.MoveTransition;
 import com.edge2.views.GeneralHeaderView;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.chip.ChipGroup;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
@@ -52,10 +53,8 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import ir.androidexception.datatable.DataTable;
 import ir.androidexception.datatable.model.DataTableHeader;
@@ -64,6 +63,7 @@ import ir.androidexception.datatable.model.DataTableRow;
 public class CAFragment extends BaseFragment {
     private static String URL_BROCHURE = "https://drive.google.com/open?id=10MTheTeHReLNGVpPAl1sFkjaRcT8pAY7";
     private static String URL_REGISTER = "https://forms.gle/vujB6gvrsVL8W8Xn9";
+    private static String KEY_IS_POINTS_SHOWING = "isPointsShowing";
 
     private OnSharedElementListener sharedElementListener;
     private MoveTransition transition;
@@ -76,10 +76,18 @@ public class CAFragment extends BaseFragment {
     private GeneralHeaderView topView;
     private TextView dataPoints;
     private DataTable dataTable;
+    private DataTable rulesTable;
+    private ChipGroup pointsChipGroup;
+    private View pointsHolder;
+    private View rulesHolder;
+    private View perksHolder;
     private int animTime;
     private int animOffset;
     private OnAuthStartListener authStartListener;
     private boolean isTransitionPlayed;
+    private int caScreenNumber;
+
+    //TODO: Break this Fragment up into different Fragments. I just didn't have the time.
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -116,6 +124,11 @@ public class CAFragment extends BaseFragment {
         dataHolderView = view.findViewById(R.id.ca_data_holder);
         dataPoints = view.findViewById(R.id.ca_data_points);
         dataTable = view.findViewById(R.id.ca_data_table);
+        rulesTable = view.findViewById(R.id.ca_rules_table);
+        pointsChipGroup = view.findViewById(R.id.ca_points_chipgroup);
+        pointsHolder = view.findViewById(R.id.ca_points_holder);
+        rulesHolder = view.findViewById(R.id.ca_rules_holder);
+        perksHolder = view.findViewById(R.id.ca_perks_holder);
         errorView = view.findViewById(R.id.ca_error_holder);
         loginPrompt = view.findViewById(R.id.ca_account_prompt);
 
@@ -147,7 +160,27 @@ public class CAFragment extends BaseFragment {
         if (savedInstanceState != null) {
             topView.showImage(0);
             isTransitionPlayed = true;
+            caScreenNumber = savedInstanceState.getInt(KEY_IS_POINTS_SHOWING);
         }
+        if (caScreenNumber == 0) {
+            hideView(rulesHolder);
+            hideView(perksHolder);
+            showView(pointsHolder);
+        } else if (caScreenNumber == 1) {
+            hideView(pointsHolder);
+            hideView(perksHolder);
+            showView(rulesHolder);
+        } else {
+            hideView(rulesHolder);
+            hideView(pointsHolder);
+            showView(perksHolder);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_IS_POINTS_SHOWING, caScreenNumber);
     }
 
     private void setUserInfo() {
@@ -215,6 +248,11 @@ public class CAFragment extends BaseFragment {
         topView = null;
         dataPoints = null;
         dataTable = null;
+        pointsHolder = null;
+        pointsChipGroup = null;
+        rulesHolder = null;
+        rulesTable = null;
+        perksHolder = null;
     }
 
     private void setupListeners(View rootView) {
@@ -242,6 +280,24 @@ public class CAFragment extends BaseFragment {
                         setData();
                     });
                 }
+            }
+        });
+        pointsChipGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.ca_points_chip) {
+                caScreenNumber = 0;
+                hideView(perksHolder);
+                hideView(rulesHolder);
+                showView(pointsHolder);
+            } else if (checkedId == R.id.ca_rules_chip) {
+                caScreenNumber = 1;
+                hideView(perksHolder);
+                hideView(pointsHolder);
+                showView(rulesHolder);
+            } else if (checkedId == R.id.ca_perks_chip) {
+                caScreenNumber = 2;
+                hideView(rulesHolder);
+                hideView(pointsHolder);
+                showView(perksHolder);
             }
         });
     }
@@ -396,14 +452,10 @@ public class CAFragment extends BaseFragment {
         Object name = document.get("name");
         if (name instanceof String)
             topView.setName((String) name);
-        Object points = document.get("points");
-        if (points instanceof Number)
-            dataPoints.setText(
-                    String.format(
-                            getString(R.string.ca_points_template), ((Number) points).intValue()));
 
         try {
             setupTable(document);
+            setupRulesTable();
         } catch (ClassCastException e) {
             e.printStackTrace();
             showNoTable();
@@ -415,68 +467,104 @@ public class CAFragment extends BaseFragment {
         //TODO: Show YET ANOTHER damn view!
     }
 
-    @SuppressWarnings("unchecked")
     private void setupTable(DocumentSnapshot document) throws ClassCastException {
         Object data = document.get("data");
-        if ((data instanceof Map)) {
-            Map tableData = (Map) data;
-            List<String> headers = (List<String>) tableData.get("head");
-            List<String> rowHeaders = (List<String>) tableData.get("rowHead");
-            Map<String, List<String>> rows = (Map<String, List<String>>) tableData.get("rows");
+        if (data instanceof List) {
+            List<Integer> tableData = convertToIntList((List) data);
 
-            if (headers == null || headers.size() == 0
-                    || rowHeaders == null || rowHeaders.size() == 0
-                    || rows == null || rows.size() == 0) {
-                showNoTable();
-                return;
-            }
-
-            Set<String> rowsSet = rows.keySet();
-            List<String> rowsSorted = new ArrayList<>(rowsSet);
-            Collections.sort(rowsSorted);
-
-            showTable(headers, rowHeaders, rowsSorted, rows);
+            // Not translatable; I don't care. Move it to Strings if you do.
+            String[] headers = {"PARTICULARS", "POINTS"};
+            String[] rowHeaders = {"Poster", "Merchandise", "Workshop [on 28-29th Mar]", "Strokes",
+                    "Coupon(s)", "EDGE event registration", "Workshop [17-19th Apr]"};
+            showTable(headers, rowHeaders, tableData);
         } else {
             showNoTable();
         }
     }
 
-    private void showTable(List<String> columnHeaders, List<String> rowHeaders, List<String> rowNames,
-                           Map<String, List<String>> rows) {
-        DataTableHeader.Builder tableHeaders = new DataTableHeader.Builder();
-        ArrayList<DataTableRow> tableRows = new ArrayList<>();
-        tableHeaders.item("", 1); // Dummy column to accommodate row headers
-        for (String header : columnHeaders) {
-            tableHeaders.item(header, 1);
+    // Using this just in case I forget to change the data type to Number in Firebase while entering
+    // data. Will remove this after writing a script to fill the values instead of the FB Console.
+    private List<Integer> convertToIntList(List data) throws ClassCastException {
+        List<Integer> points = new LinkedList<>();
+        for (Object item : data) {
+            if (item instanceof Number) {
+                points.add(((Number) item).intValue());
+            } else if (item instanceof String) {
+                points.add(Integer.parseInt((String) item));
+            } else {
+                throw new ClassCastException();
+            }
+        }
+        return points;
+    }
+
+    private void showTable(String[] columnHeaders, String[] rowHeaders, List<Integer> data) {
+        if (data == null || data.size() != rowHeaders.length) {
+            // We messed up during data entry.
+            showSingleView(errorView);
+            Context context = getContext();
+            if (context != null)
+                Toast.makeText(context,
+                        "Inconsistent data. Please contact Geekonix.", Toast.LENGTH_LONG)
+                        .show();
+            return;
         }
 
-        for (int i = 0; i < rowNames.size(); i++) {
-            String rowName = rowNames.get(i);
-            List<String> row = rows.get(rowName);
-            if (row == null || row.size() != columnHeaders.size()) {
-                // We messed up during data entry.
-                showSingleView(errorView);
-                Context context = getContext();
-                if (context != null)
-                    Toast.makeText(context,
-                            "Inconsistent data. Please contact Geekonix.", Toast.LENGTH_LONG)
-                            .show();
-                return;
-            }
+        DataTableHeader.Builder tableHeaders = new DataTableHeader.Builder();
+        ArrayList<DataTableRow> tableRows = new ArrayList<>();
+        for (int i = 0; i < columnHeaders.length; i++) {
+            int weight;
+            if (i == 0)
+                weight = 1;
+            else
+                weight = 1;
+            tableHeaders.item(columnHeaders[i], weight);
+        }
+
+        int totalPoints = 0;
+        for (int i = 0; i < rowHeaders.length; i++) {
+            totalPoints += data.get(i);
             DataTableRow.Builder tableRow = new DataTableRow.Builder();
-            tableRow.value(rowHeaders.get(i));    // Setting the row header
-            for (String item : row) {
-                tableRow.value(item);
-            }
+            tableRow.value(rowHeaders[i]);
+            tableRow.value(String.valueOf(data.get(i)));
             tableRows.add(tableRow.build());
         }
 
-        dataTable.setTypeface(ResourcesCompat.getFont(getContext(), R.font.forum));
+        dataPoints.setText(
+                String.format(
+                        getString(R.string.ca_points_template), totalPoints));
+
         dataTable.setHeader(tableHeaders.build());
         dataTable.setRows(tableRows);
         Context context = getContext();
-        if (context != null)
+        if (context != null) {
+            dataTable.setTypeface(ResourcesCompat.getFont(context, R.font.forum));
             dataTable.inflate(context);
+        }
+    }
+
+    private void setupRulesTable() {
+        ArrayList<DataTableRow> tableRows = new ArrayList<>();
+        String[] rowHeaders = getResources().getStringArray(R.array.ca_rule_evs);
+        String[] data = getResources().getStringArray(R.array.ca_rule_pts);
+        DataTableHeader.Builder tableHeaders = new DataTableHeader.Builder()
+                .item("PARTICULARS", 2)
+                .item("POINTS", 1);
+
+        for (int i = 0; i < rowHeaders.length; i++) {
+            DataTableRow.Builder tableRow = new DataTableRow.Builder();
+            tableRow.value(rowHeaders[i]);
+            tableRow.value(String.valueOf(data[i]));
+            tableRows.add(tableRow.build());
+        }
+
+        rulesTable.setHeader(tableHeaders.build());
+        rulesTable.setRows(tableRows);
+        Context context = getContext();
+        if (context != null) {
+            rulesTable.setTypeface(ResourcesCompat.getFont(context, R.font.forum));
+            rulesTable.inflate(context);
+        }
     }
 
     private class OnSharedElementListener implements Transition.TransitionListener {
